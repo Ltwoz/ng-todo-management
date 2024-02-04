6384acec2dd7509533c34176928ed3e842e60cf7
Ltwoz <warrapat.102@gmail.com>
2024-02-04 23:12:02 +0700

    guard and interceptor

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { ILogin, ILoginResponse } from '../models/auth.model';
+import { HttpClient } from '@angular/common/http';
+import { apiEndpoint } from '../constants/constants';
+import { map } from 'rxjs';
+import { TokenService } from './token.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private http: HttpClient, private tokenService: TokenService) {}
 
-  constructor() { }
+  onLogin(data: ILogin) {
+    return this.http
+      .post<ILoginResponse>(apiEndpoint.AuthEndpoint.login, data)
+      .pipe(
+        map((response) => {
+          if (response) {
+            this.tokenService.setToken(response.token);
+          }
+          return response;
+        })
+      );
+  }
+
+  onLogout() {
+    this.http.post(apiEndpoint.AuthEndpoint.logout, '').subscribe({
+      next: (response) => {
+        this.tokenService.removeToken();
+      },
+    });
+  }
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,26 +1,35 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
   loginForm!: FormGroup;
 
-  constructor(private fb: FormBuilder, ) {
+  constructor(private fb: FormBuilder, private authService: AuthService) {
     this.loginForm = this.fb.group({
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required])
-    })
+      password: new FormControl('', [Validators.required]),
+    });
   }
 
   onSubmit() {
     if (this.loginForm.valid) {
-
+      this.authService.onLogin(this.loginForm.value).subscribe({
+        next: () => {},
+      });
     } else {
       this.loginForm.markAllAsTouched();
     }
